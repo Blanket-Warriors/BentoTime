287ca7792e2f8632ad8c346ec4ae5a8685ded8e2
Ben Pevsner <ivebencrazy@users.noreply.github.com>
2016-06-15 09:17:56 -0700

    [test] Adds test for booklist date ordering

--- app/renderer-process/components/BookList/BookList_Tests.jsx
+++ app/renderer-process/components/BookList/BookList_Tests.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 import { shallow } from "enzyme";
 import BookList from "./BookList";
 
@@ -8,51 +9,60 @@ describe("Components", function() {
       this.books = {
         "flappy-monkey": {
           id: "flappy-monkey",
-          title: "Flappy Monkey Banana Attack"
+          title: "Flappy Monkey Banana Attack",
+          lastChapterDate: moment().add(-5, "hours")
         },
         "flappy-monkey-2": {
           id: "flappy-monkey-2",
-          title: "Return of the Flappy Monkey"
+          title: "Return of the Flappy Monkey",
+          lastChapterDate: moment()
         }
       };
 
       this.component = shallow(<BookList books={this.books} />);
     });
 
-    it("Should render an `ul` tag", function renderUl() {
+    it("Should render an `ul` tag", function() {
       expect(this.component.type()).to.equal("ul");
       expect(this.component.hasClass("book-list")).to.be.true;
     });
 
-    it("Should not any books if filter prop is empty", function emptyProp() {
+    it("Should not any books if filter prop is empty", function() {
       expect(this.component.find("BookListItem")).have.length(0);
     });
 
-    it("Should display no books if filter prop is false", function falseProp() {
+    it("Should display no books if filter prop is false", function() {
       this.component = shallow(<BookList books={this.books} searchFilter={false} />);
       expect(this.component.find("BookListItem")).to.have.length(0);
     });
 
-    it("Should display all books if filter prop is true", function trueProp() {
+    it("Should display all books if filter prop is true", function() {
       this.component = shallow(<BookList books={this.books} searchFilter={true} />);
       expect(this.component.find("BookListItem")).have.length(2);
     });
 
-    it("Should filter titles by string", function filterBookListByString() {
+    it("Should filter titles by string", function() {
       this.component = shallow(<BookList books={this.books} searchFilter="Return of the" />);
       expect(this.component.find("BookListItem")).to.have.length(1);
     });
 
-    it("Should filter at the beginning of the title if filter is less than 3 characters", function filterBookListByString() {
+    it("Should filter at the beginning of the title if filter is less than 3 characters", function() {
       this.component = shallow(<BookList books={this.books} searchFilter="Fla" />);
       expect(this.component.find("BookListItem")).to.have.length(1);
     });
 
-    it("Should not display all books if filter prop is the string true", function trueStringProp() {
+    it("Should not display all books if filter prop is the string true", function() {
       this.component = shallow(<BookList books={this.books} searchFilter={"true"} />);
       expect(this.component.find("BookListItem")).to.have.length(0);
     });
 
-    it("Should sort with newest updates at the top");
+    it("Should sort with newest updates at the top", function() {
+      this.component = shallow(<BookList books={this.books} searchFilter={true} />);
+      const bookList = this.component.find(".book-list");
+      const firstBook = bookList.childAt(0).prop("book");
+      const secondBook = bookList.childAt(1).prop("book");
+      expect(firstBook.id).to.equal("flappy-monkey-2");
+      expect(secondBook.id).to.equal("flappy-monkey");
+    });
   });
 });
