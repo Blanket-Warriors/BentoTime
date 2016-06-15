aa81adfc134040f1e630114861fd1726b2d495ed
Ben Pevsner <ivebencrazy@users.noreply.github.com>
2016-06-14 21:38:50 -0700

    [test] Adds unit tests for `shouldUpdate`

--- app/renderer-process/utilities/shouldUpdate/shouldUpdate.js
+++ app/renderer-process/utilities/shouldUpdate/shouldUpdate.js
@@ -1,19 +1,16 @@
 import moment, { isMoment } from "moment";
 
 export default function shouldUpdate(toCheck) {
-  if(!toCheck) {
-    return false;
-  }
+  if(!toCheck) { return false; }
+  if(!toCheck.lastUpdated) { return true; }
 
-  if(!toCheck.lastUpdated) {
-    return true;
-  }
   var lastUpdated;
   if(isMoment(toCheck.lastUpdated)) {
     lastUpdated = toCheck.lastUpdated;
   } else {
     lastUpdated = moment.unix(toCheck.lastUpdated);
   }
+
   const threeHoursAgo = moment().add(-2, "hours");
   return lastUpdated.isBefore(threeHoursAgo, "hour");
 }
--- app/renderer-process/utilities/shouldUpdate/shouldUpdate__Tests.js
+++ app/renderer-process/utilities/shouldUpdate/shouldUpdate__Tests.js
@@ -1,12 +1,34 @@
+import moment from "moment";
 import shouldUpdate from "./shouldUpdate";
 
 describe("Utilities", function describeUtilities() {
   describe("shouldUpdate", function describeShouldUpdate() {
-    it("should update if toCheck doesn't exist");
-    it("should update if toCheck does not have a lastUpdated property");
-    it("should update if toCheck is out of date with a string date");
-    it("should update if toCheck is out of date with a moment.js object date");
-    it("should not update if toCheck is recent with a string date");
-    it("should not update if toCheck is recent with a moment.js object date");
+    it("should not update if toCheck doesn't exist", function() {
+      expect(shouldUpdate()).to.be.false;
+    });
+
+    it("should update if toCheck does not have a lastUpdated property", function() {
+      expect(shouldUpdate({})).to.be.true;
+    });
+
+    it("should update if toCheck is out of date", function() {
+      expect(shouldUpdate({
+        lastUpdated: moment().add(-1, "days")
+      })).to.be.true;
+
+      expect(shouldUpdate({
+        lastUpdated: moment().add(-1, "days").unix()
+      })).to.be.true;
+    });
+
+    it("should not update if toCheck is recent", function() {
+      expect(shouldUpdate({
+        lastUpdated: moment()
+      })).to.be.false;
+
+      expect(shouldUpdate({
+        lastUpdated: moment().unix()
+      })).to.be.false;
+    });
   });
 });
