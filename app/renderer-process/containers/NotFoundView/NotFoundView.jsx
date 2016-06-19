eb1fce7fecc1cafb5d07693d914deaef976574fb
Ben Pevsner <ivebencrazy@users.noreply.github.com>
2016-06-19 13:35:04 -0700

    [tidy] Updates Not Found View

--- app/renderer-process/containers/NotFoundView/NotFoundView.jsx
+++ app/renderer-process/containers/NotFoundView/NotFoundView.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import combine from "renderer/utilities/combineClasses";
 
-const NotFoundView = function NotFoundView() {
-  return <h1>NotFound View</h1>;
+const NotFoundView = function NotFoundView({ className }) {
+  return (
+    <h3 className={combine("not-found-view", className)}>
+      Page Not Found
+    </h3>
+  );
+};
+
+NotFoundView.propTypes = {
+  className: React.PropTypes.string
+};
+
+NotFoundView.defaultProps = {
+  className: ""
 };
 
 export default NotFoundView;
