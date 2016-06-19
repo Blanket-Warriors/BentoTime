3e9566c357e074d4731605319b243dbf79729692
Ben Pevsner <ivebencrazy@users.noreply.github.com>
2016-06-19 11:16:25 -0700

    [change] Updates BookView README

--- app/renderer-process/containers/BookView/BookView.jsx
+++ app/renderer-process/containers/BookView/BookView.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import combine from "renderer/utilities/combineClasses";
 
 import BookHero from "renderer/components/BookHero";
 import BookInfo from "renderer/components/BookInfo";
 
-const BookView = function BookView({ book, dispatch }) {
+const BookView = function BookView({ book, dispatch, className }) {
   if(!book || !book.chapters || book.isFetching) {
-    return <h3 className="book-view--loading">loading...</h3>;
+    return (
+      <h3 className={combine("book-view book-view--loading", className)}>
+        loading...
+      </h3>
+    );
   }
 
   return (
-    <div className="book-view">
+    <div className={combine("book-view", className)}>
       <BookHero className="book-view__hero" book={book} dispatch={dispatch} />
       <BookInfo className="book-view__info" book={book}/>
     </div>
   );
+};
+
+BookView.propTypes = {
+  book: React.PropTypes.object.isRequired,
+  dispatch: React.PropTypes.func.isRequired,
+  className: React.PropTypes.string
+};
+
+BookView.defaultProps = {
+  book: null,
+  dispatch: function(){},
+  className: ""
 };
 
 export default BookView;
